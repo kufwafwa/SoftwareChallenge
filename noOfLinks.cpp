1e9ebb5cae862effe60e42ff053f1492aa72ae0d
kufwafwa <kufwafwanancy@yahoo.com>
2017-12-11 04:32:53 +0100

    modified socialNetworkSize.cpp

--- noOfLinks.cpp
+++ noOfLinks.cpp
@@ -5,80 +5,111 @@
 #include <set>
 #include <cstdlib>
 
-using namespace std;
+#define FIRST_NAME "STACEY_STRIMPLE"
+#define SECOND_NAME "RICH_OMLI"
+
+//Function declaration
+void count_no_of_links(std::ifstream&, std::string, std::string);
+//count_no_of_links - counts the shortest number of links between 2 friends in a social network
 
 int main()
 {
-    ifstream inFile;
+    std::ifstream infile;
+    std::string firstname, secondname;
+#ifdef FIRST_NAME
+    firstname = FIRST_NAME;
+#else
+    std::cout<<"Please enter the first name : ";
+    std::cin>>firstname;
+#endif // NAME_A
+#ifdef SECOND_NAME
+    secondname = SECOND_NAME;
+#else
+    std::cout<<"Please enter the second name : ";
+    std::cin>>secondname;
+#endif
+    count_no_of_links(infile,firstname,secondname);
+    std::cin.get();
+    return 0;
+}
+
+//Function definition
+void count_no_of_links(std::ifstream& inFile, std::string first_name, std::string second_name)
+{
     inFile.open("SocialNetwork.txt");
     if(inFile.fail())
     {
-        cerr<<"File cannot open"<<endl;
+        std::cerr<<"File cannot open"<<std::endl;
         exit(1);
     }
-    vector<string> socialNetwork;
-    set<string> check, nextCheck;
-    set<string>::iterator it;
-    string firstName, secondName;
-    while(!inFile.eof())
+
+    if (second_name.compare(first_name)==0)
     {
-        getline(inFile,firstName,',');
-        getline(inFile,secondName,'\n');
-        socialNetwork.push_back(firstName);
-        socialNetwork.push_back(secondName);
-    }
-    firstName = "STACEY_STRIMPLE";
-    secondName= "RICH_OMLI";
-    check.insert(firstName);
-    it = check.begin();
-    int noOfLinks=1;
-    bool found=false, notLinked=false;
-    if (secondName.compare(firstName)==0)
-    {
-        cout<<"A and B are the same person "<<endl;
+        std::cout<<first_name<<" and "<<second_name<<" are the same person "<<std::endl;
     }
     else
     {
+        std::vector<std::string> socialNetwork;
+        std::string left_name,right_name;
+        while(!inFile.eof())
+        {
+            getline(inFile,left_name,',');
+            getline(inFile,right_name,'\n');
+            socialNetwork.push_back(left_name);
+            socialNetwork.push_back(right_name);
+        }
+        std::set<std::string> current_level_links, next_level_links;
+        std::set<std::string>::iterator name_to_check;
+        std::string name;
+        unsigned int no_of_links=0;
+
+        current_level_links.insert(first_name);
+        name_to_check = current_level_links.begin();
+        bool found=false, no_link=false;
         while(found==false)
         {
-            if(socialNetwork.empty()!=true)
+            if(socialNetwork.size()==0)
             {
-                while(check.empty() !=true)
+                std::cout<<"The social network is empty"<<std::endl;
+                no_link=true;
+                break;
+            }
+            else
+            {
+                while(current_level_links.size()!=0)
                 {
-                    it = check.begin();
-                    firstName=*it;
-                    check.erase (it);
-
+                    name_to_check = current_level_links.begin();
+                    name=*name_to_check;
+                    current_level_links.erase (name_to_check);
                     for (unsigned i=1; i < socialNetwork.size()+1; i++)
                     {
-
-                        if(socialNetwork[i-1].compare(firstName)==0)
+                        if(socialNetwork[i-1].compare(name)==0)
                         {
-                            if((i-1)%2==0)
+                            if(i%2==0)
                             {
-                                if(socialNetwork[i+1-1].compare(secondName)==0)
+                                if(socialNetwork[i-2].compare(second_name)==0)
                                 {
                                     found=true;
-                                    i=socialNetwork.size()+1;
+                                    break;
                                 }
                                 else
                                 {
-                                    nextCheck.insert(socialNetwork[i+1-1]);
-                                    socialNetwork.erase(socialNetwork.begin()+i+1-1);
-                                    socialNetwork.erase(socialNetwork.begin()+i+1-1);
+                                    next_level_links.insert(socialNetwork[i-2]);
+                                    socialNetwork.erase(socialNetwork.begin()+i-1);
+                                    socialNetwork.erase(socialNetwork.begin()+i-1);
                                     i--;
                                 }
                             }
                             else
                             {
-                                if(socialNetwork[i-1-1].compare(secondName)==0)
+                                if(socialNetwork[i].compare(second_name)==0)
                                 {
                                     found=true;
-                                    i=socialNetwork.size()+1;
+                                    break;
                                 }
                                 else
                                 {
-                                    nextCheck.insert(socialNetwork[i-1-1]);
+                                    next_level_links.insert(socialNetwork[i]);
                                     socialNetwork.erase(socialNetwork.begin()+i-1);
                                     socialNetwork.erase(socialNetwork.begin()+i-1);
                                     i--;
@@ -86,36 +117,28 @@ int main()
                             }
                         }
                     }
-
                 }
-                if(nextCheck.empty()==true)
-                {
-                    notLinked=true;
-                    found=true;
-                }
-                else
-                {
-                    check=nextCheck;
-                    nextCheck.clear();
-                    noOfLinks++;
-                }
-
+            }
+            if(next_level_links.size()!=0 )
+            {
+                current_level_links=next_level_links;
+                next_level_links.clear();
+                no_of_links++;
             }
             else
             {
-                notLinked=true;
-                found=true;
+                if (found!=true)
+                {
+                    no_of_links=0;
+                }
+                else
+                {
+                    no_of_links++;
+                }
+                socialNetwork.clear();
+                break;
             }
         }
-
-        if(notLinked)
-        {
-            cout<<"A and B are not linked "<<endl;
-        }
-        else
-        {
-            cout<<"The distance between A and B is : "<<noOfLinks<< " links"<<endl;
-        }
+        std::cout<<"The distance between "<< first_name<<" and "<<second_name<<" is : "<<no_of_links<< " link(s)"<<std::endl;
     }
-    return 0;
 }
--- socialNetworkSize.cpp
+++ socialNetworkSize.cpp
@@ -4,23 +4,29 @@
 #include <set>
 #include <cstdlib>
 
-using namespace std;
+//Function declaration
+void count_people_in_social_network(std::ifstream&);
+//count_people_in_social_network - counts the number of people in a social network
 int main(){
-    ifstream inFile;
-    inFile.open("SocialNetwork.txt");
-    if(inFile.fail()){
-        cerr<<"File cannot open"<<endl;
-        exit(1);
-    }
-    set<string> socialNetwork;
-    string firstName, secondName;
-    while(!inFile.eof()){
-        getline(inFile,firstName,',');
-        getline(inFile,secondName,'\n');
-        socialNetwork.insert(firstName);
-        socialNetwork.insert(secondName);
-    }
-    cout<<"The total number of people in the social network is : "<<socialNetwork.size();
+    std::ifstream infile;
+    infile.open("SocialNetwork.txt");
+    count_people_in_social_network(infile);
+    std::cin.get();
     return 0;
 }
-
+//Function definition
+void count_people_in_social_network(std::ifstream& inFile){
+   if(inFile.fail()){
+        std::cerr<<"File cannot open"<<std::endl;
+        exit(1);
+    }
+    std::set<std::string> socialNetwork;
+    std::string left_name, right_name;
+    while(!inFile.eof()){
+        getline(inFile,left_name,',');
+        getline(inFile,right_name,'\n');
+        socialNetwork.insert(left_name);
+        socialNetwork.insert(right_name);
+    }
+    std::cout<<"The total number of people in the social network is : "<<socialNetwork.size()<<std::endl;
+}
